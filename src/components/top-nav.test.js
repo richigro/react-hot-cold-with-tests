bff00b1d3cf1024c1141ef574791876ce373976b
ricardo guerrero <richi_gro@hotmail.com>
2019-08-06 14:57:17 -0500

    adding new test for statelss and stateful components

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import shallow from 'enzyme';
+import {shallow} from 'enzyme';
 
 import TopNav from './top-nav';
 
 describe('<TopNav/>', () => {
-    it('Sallow render'), () => {
-        shallow(<TopNav/>);
-    };
+    it('should contain three li elements', () => {
+        const wrapper = shallow(<TopNav/>);
+        // console.log(wrapper.debug());
+        expect(wrapper.contains('nav')).to.have.lengthOf(1);
+      });
 });
